a3f550f60a96da28d70af4da0d9a04aa2fe2366d
scolpig <scolpig@gmail.com>
2020-10-13 12:15:24 +0900

    first

---
+++ exam23_ml09_Beauty_GAN.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import dlib 
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import numpy as np
+
+
+# In[10]:
+
+
+detector = dlib.get_frontal_face_detector()
+sp = dlib.shape_predictor(
+    './models/shape_predictor_5_face_landmarks.dat')
+
+
+# In[11]:
+
+
+img = dlib.load_rgb_image('./imgs/12.jpg')
+plt.figure(figsize=(16,10))
+plt.imshow(img)
+plt.show()
+
+
+# In[12]:
+
+
+img_result = img.copy()
+dets = detector(img)
+if len(dets) == 0:
+    print('connot find faces!')
+else:
+    fig, ax = plt.subplots(1,figsize=(16,10))
+    for det in dets:
+        x, y, w, h = det.left(), det.top(), det.width(), det.height()
+        rect = patches.Rectangle((x,y),w,h,linewidth=2, edgecolor='r',
+                                facecolor='none')
+        ax.add_patch(rect)
+    ax.imshow(img_result)
+    plt.show()
+
+
+# In[13]:
+
+
+fig, ax = plt.subplots(1, figsize=(16,10))
+objs = dlib.full_object_detections()
+for detection in dets:
+    s = sp(img, detection)
+    objs.append(s)
+    for point in s.parts():
+        circle = patches.Circle((point.x, point.y), radius=3,
+                               edgecolor='r', facecolor='r')
+        ax.add_patch(circle)
+ax.imshow(img_result)
+plt.show()
+
+# In[14]:
+
+
+faces = dlib.get_face_chips(img, objs, size=256, padding=0.3)
+fig, axes = plt.subplots(1, len(faces)+1, figsize=(20,16))
+axes[0].imshow(img)
+for i, face in enumerate(faces):
+    axes[i+1].imshow(face)
+plt.show()
+
+# In[15]:
+
+
+def align_faces(img):
+    dets = detector(img, 1)
+    objs = dlib.full_object_detections()
+    for detection in dets:
+        s = sp(img, detection)
+        objs.append(s)
+    faces = dlib.get_face_chips(img, objs, size=256, padding=0.35)
+    return faces
+test_img = dlib.load_rgb_image('./imgs/17.jpg')
+test_faces = align_faces(test_img)
+fig, axes = plt.subplots(1, len(test_faces)+1, figsize=(20,16))
+axes[0].imshow(test_img)
+for i, face in enumerate(test_faces):
+    axes[i+1].imshow(face)
+plt.show()
+
+# In[16]:
+
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+saver = tf.train.import_meta_graph('./models/model.meta')
+saver.restore(sess, tf.train.latest_checkpoint('./models'))
+graph = tf.get_default_graph()
+X = graph.get_tensor_by_name('X:0')
+Y = graph.get_tensor_by_name('Y:0')
+Xs = graph.get_tensor_by_name('generator/xs:0')
+
+
+# In[17]:
+
+
+def preprocess(img):
+    return (img / 255. - 0.5) * 2
+
+def deprocess(img):
+    return (img + 1) / 2
+
+
+# In[30]:
+
+
+img1 = dlib.load_rgb_image('./imgs/12.jpg')
+img1_faces = align_faces(img1)
+
+img2 = dlib.load_rgb_image('./imgs/makeup/vFG56.png')
+img2_faces = align_faces(img2)
+
+fig, axes = plt.subplots(1,2,figsize=(16,10))
+axes[0].imshow(img1_faces[0])
+axes[1].imshow(img2_faces[0])
+plt.show()
+
+
+# In[31]:
+
+
+src_img = img1_faces[0]
+ref_img = img2_faces[0]
+
+X_img = preprocess(src_img)
+X_img = np.expand_dims(X_img, axis=0)
+
+Y_img = preprocess(ref_img)
+Y_img = np.expand_dims(Y_img, axis=0)
+
+output = sess.run(Xs, feed_dict={X:X_img, Y:Y_img})
+output_img = deprocess(output[0])
+
+fig, axes = plt.subplots(1, 3, figsize=(20,10))
+axes[0].set_title('Source')
+axes[0].imshow(src_img)
+axes[1].set_title('Reference')
+axes[1].imshow(ref_img)
+axes[2].set_title('Result')
+axes[2].imshow(output_img)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
